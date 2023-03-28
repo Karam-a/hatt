765dcf21e14f854ff1ca5e63ada04478e4a4ac24
Karam-a <81566216+Karam-a@users.noreply.github.com>
2023-03-28 10:01:42 +0200

    Javakod Uppladdad

---
+++ HattmakarenTeam2/src/hattmakarenteam2/HattmakarenTeam2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hattmakarenteam2;
+
+/**
+ *
+ * @author Karam Al-Akhras
+ */
+public class HattmakarenTeam2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
