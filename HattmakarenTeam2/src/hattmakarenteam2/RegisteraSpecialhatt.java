3c96e71d186d3750c6ff83202acddf0fce0363ba
portuguesellen <105210458+portuguesellen@users.noreply.github.com>
2023-04-04 12:27:40 +0200

    h

--- HattmakarenTeam2/src/hattmakarenteam2/RegisteraSpecialhatt.java
+++ HattmakarenTeam2/src/hattmakarenteam2/RegisteraSpecialhatt.java
@@ -26,17 +26,94 @@ public class RegisteraSpecialhatt extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        SpecialhattLabel = new javax.swing.JLabel();
+        HattIDLabel = new javax.swing.JLabel();
+        NamnLabel = new javax.swing.JLabel();
+        TygLabel = new javax.swing.JLabel();
+        StorlekLabel = new javax.swing.JLabel();
+        ModellLabel = new javax.swing.JLabel();
+        FärgLabel = new javax.swing.JLabel();
+        DekorationLabel = new javax.swing.JLabel();
+        TextLabel = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        SpecialhattLabel.setText("Specialhatt");
+        SpecialhattLabel.setPreferredSize(new java.awt.Dimension(80, 17));
+
+        HattIDLabel.setText("HattID:");
+
+        NamnLabel.setText("Namn:");
+
+        TygLabel.setText("Tyg:");
+
+        StorlekLabel.setText("Storlek:");
+
+        ModellLabel.setText("Modell:");
+
+        FärgLabel.setText("Färg: ");
+
+        DekorationLabel.setText("Dekoration:");
+
+        TextLabel.setText("Text:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(150, 150, 150)
+                            .addComponent(SpecialhattLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(31, 31, 31)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(HattIDLabel)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(TygLabel)
+                                    .addComponent(NamnLabel))))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(26, 26, 26)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(FärgLabel)
+                                        .addComponent(ModellLabel)))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addContainerGap()
+                                    .addComponent(StorlekLabel)))
+                            .addGap(161, 161, 161)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(TextLabel)
+                            .addComponent(DekorationLabel))))
+                .addContainerGap(170, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(SpecialhattLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(HattIDLabel)
+                .addGap(18, 18, 18)
+                .addComponent(NamnLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(TygLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(StorlekLabel)
+                .addGap(18, 18, 18)
+                .addComponent(ModellLabel)
+                .addGap(18, 18, 18)
+                .addComponent(FärgLabel)
+                .addGap(18, 18, 18)
+                .addComponent(DekorationLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(TextLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -44,5 +121,14 @@ public class RegisteraSpecialhatt extends javax.swing.JFrame {
 
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel DekorationLabel;
+    private javax.swing.JLabel FärgLabel;
+    private javax.swing.JLabel HattIDLabel;
+    private javax.swing.JLabel ModellLabel;
+    private javax.swing.JLabel NamnLabel;
+    private javax.swing.JLabel SpecialhattLabel;
+    private javax.swing.JLabel StorlekLabel;
+    private javax.swing.JLabel TextLabel;
+    private javax.swing.JLabel TygLabel;
     // End of variables declaration//GEN-END:variables
 }
