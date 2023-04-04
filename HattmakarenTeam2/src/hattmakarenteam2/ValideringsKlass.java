52bafebca6879b87f48692c831bcb7f0b8315565
PotRoastPiggy <alexmcc99@yahoo.se>
2023-04-04 12:24:31 +0200

    Valideringsklassmetod

--- HattmakarenTeam2/src/hattmakarenteam2/ValideringsKlass.java
+++ HattmakarenTeam2/src/hattmakarenteam2/ValideringsKlass.java
@@ -9,5 +9,10 @@ package hattmakarenteam2;
  * @author alexm
  */
 public class ValideringsKlass {
-    
+   //Använde den här i mitt MIB
+    public static void isNullValue(String result, String typeObject) throws IDException{
+        if(result != null && result != ""){
+            throw new IDException("This " +typeObject + "Id already exists ");
+        }
+    }
 }
