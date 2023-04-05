e44633300de14d745b4df03eb35beb4ac9076ae2
portuguesellen <105210458+portuguesellen@users.noreply.github.com>
2023-04-05 10:33:11 +0200

    h

--- HattmakarenTeam2/src/hattmakarenteam2/ValideringsKlass.java
+++ HattmakarenTeam2/src/hattmakarenteam2/ValideringsKlass.java
@@ -9,6 +9,39 @@ import javax.swing.JTextField;
 /**
  *
  * @author alexm
+ * 
+ * 
+ *    
+    public static boolean textFaltHarVarde(JTextField rutaAttKolla)
+    {
+        boolean resultat = true; 
+        
+        if(rutaAttKolla.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Inmatningsrutan ‰r tom");
+            resultat = false;
+        }
+        
+        return resultat;
+        
+    }
+    * 
+    * 
+    * 
+    public static boolean datumKontroll (String datum){
+       SimpleDateFormat datumformat = new SimpleDateFormat ("yyyy-mm-dd");
+       datumformat.setLenient(false);
+       
+        try{
+            Date datumAttKolla = datumformat.parse(datum);
+            return true;
+        }
+        
+        catch(ParseException e) {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Fel format pÂ datum. Skriv det ≈≈≈≈-MM-DD");
+            return false;
+  
+  * 
  */
 public class ValideringsKlass {
      public static boolean v‰rdeExisterar(JTextField varde) {
