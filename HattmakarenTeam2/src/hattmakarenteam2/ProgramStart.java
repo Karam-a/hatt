281e3512c5138bfe290bd3e8aa1d177834ad5bc1
Salma0mer <105636109+Salma0mer@users.noreply.github.com>
2023-04-11 08:31:19 +0200

    l

--- HattmakarenTeam2/src/hattmakarenteam2/ProgramStart.java
+++ HattmakarenTeam2/src/hattmakarenteam2/ProgramStart.java
@@ -188,6 +188,8 @@ public class ProgramStart extends javax.swing.JFrame {
 
     private void materialBestBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_materialBestBtnActionPerformed
         // TODO add your handling code here:
+        new Materialbeställningslista(idb).setVisible(true);
+         this.dispose();
     }//GEN-LAST:event_materialBestBtnActionPerformed
 
     private void loggaInBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loggaInBtnActionPerformed
