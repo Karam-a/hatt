43992d7be0d1e4818c64120ef6be2808454c4e42
Salma0mer <105636109+Salma0mer@users.noreply.github.com>
2023-04-13 10:14:26 +0200

    l

--- HattmakarenTeam2/src/hattmakarenteam2/ValideringKlass.java
+++ HattmakarenTeam2/src/hattmakarenteam2/ValideringKlass.java
@@ -31,21 +31,7 @@ public class ValideringKlass {
     }  
     
     
-    public static boolean KontrolleraLösenord(InfDB idb, String tabel, String anvandare, JPasswordField Lösenord) {
-        boolean isRight = false;
-        try {
-            String dbLösenord = idb.fetchSingle("Select Lösenord from " + tabel + " where Användarnamn ='" + anvandare + "'");
-            if (Lösenord.equals(dbLösenord)) {
-                isRight = true;
-            } else {
-                JOptionPane.showMessageDialog(null, "Inloggning misslyckades!, Felaktig Lösenordet");
-            }
-        } catch (InfException e) {
-            JOptionPane.showMessageDialog(null, "Fel inloggningsuppgifter, försök igen.");
-                        System.out.println("Internt felmeddeland" + e.getMessage());
-        }
-        return isRight;
-    }
+    
     
         
     
