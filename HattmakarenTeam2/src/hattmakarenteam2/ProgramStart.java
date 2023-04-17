33dce5ab9389496e584c90714b7e8073a62f0607
Salma0mer <105636109+Salma0mer@users.noreply.github.com>
2023-04-17 12:30:02 +0200

    f

--- HattmakarenTeam2/src/hattmakarenteam2/ProgramStart.java
+++ HattmakarenTeam2/src/hattmakarenteam2/ProgramStart.java
@@ -247,7 +247,7 @@ public class ProgramStart extends javax.swing.JFrame {
 
     private void loggaInBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loggaInBtnActionPerformed
         // TODO add your handling code here:
-      
+     new InloggAdmin(idb).setVisible(true);
     }//GEN-LAST:event_loggaInBtnActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
